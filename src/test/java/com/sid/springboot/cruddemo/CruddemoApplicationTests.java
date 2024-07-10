33bb4503dc3ae5319ab19c2e552bd564943fdcdd
sidharth <sidharthm673@gmail.com>
2024-07-10 19:28:21 +0530

    first commit

---
+++ src/test/java/com/sid/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
