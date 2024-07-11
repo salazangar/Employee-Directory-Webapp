6fc43ff1ea576020a87edb611af9a4e5f30f4ff4
sidharth <sidharthm673@gmail.com>
2024-07-11 13:10:36 +0530

    function for deleting an employee

--- src/main/java/com/sid/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/sid/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -66,5 +66,13 @@ public class EmployeeController {
         return "employees/employee-form";
     }
 
+    @GetMapping("/delete")
+    public String deleteEmployee(@RequestParam("employeeId") int theId){
+        // delete the employee
+        employeeService.deleteById(theId);
+
+        // redirect to employee list
+        return "redirect:/employees/list";
+    }
 
 }
